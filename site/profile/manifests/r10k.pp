be18bee9c0a633661addf7e9affce88692e3b8b3
Luiz <lfelias@gmail.com>
2021-08-24 14:56:16 +1000

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,7 +2,7 @@ class profile::r10k {
   class {'r10k':
     remote => 'https://github.com/lfelias/control-repo',
    }
-   class {'r10k::webhook:config':
+   class {'r10k::webhook::config':
     use_mcollective => false,
     enable_ssl      => false,
     }
