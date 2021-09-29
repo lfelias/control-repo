ec20661bdbe843ffb0977359e8fec165cc98b002
luiz <lfelias>
2021-09-29 12:15:02 +1000

    adding hiera data

--- site/profile/manifests/ntpconfig.pp
+++ site/profile/manifests/ntpconfig.pp
@@ -11,7 +11,7 @@ class profile::ntpconfig {
 #Config File
  file { "/etc/ntp.conf":
   ensure => "present",
- content => "server 0.au.pool.ntp.org"
+ content => hiera('ntp::servers:server'),
  }
 
 
